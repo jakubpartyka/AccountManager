dfa74d60f21183bc257b98d1c861c8b1d7921270
jakubpartyka <j.partyka20@gmail.com>
2020-01-17 21:06:40 +0100

    WebTask made abstract

--- src/WebTask.java
+++ src/WebTask.java
@@ -1,33 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class WebTask implements Runnable {
+public abstract class WebTask implements Runnable {
     private List<Account> operatingAccounts = new ArrayList<>();
-    private String task;
 
-    WebTask(String task, Account account){
-        this.task = task;
+    WebTask(Account account){
         operatingAccounts.add(account);
     }
 
-    WebTask(String task, List<Account> accounts){
-        this.task = task;
+    WebTask(List<Account> accounts){
         operatingAccounts.addAll(accounts);
-    }
-
-    void discoverName(Account account){
-        System.out.println("test");
-    }
-
-    @Override
-    public void run() {
-        if(task.equalsIgnoreCase("discoverName"))
-            operatingAccounts.forEach(this::discoverName);
-        else
-            try {
-                throw new NoSuchMethodException();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            }
     }
 }
